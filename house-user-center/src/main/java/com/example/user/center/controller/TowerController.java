dfb9788cb7f1dfcc1e350d664d2a4dad18631f44
shihaoluoling <441168597@qq.com>
2021-02-25 17:02:45 +0800

    2.25

--- house-user-center/src/main/java/com/example/user/center/controller/TowerController.java
+++ house-user-center/src/main/java/com/example/user/center/controller/TowerController.java
@@ -955,12 +955,12 @@ public class TowerController {
         }
         return builder.body(ResponseUtils.getResponseBody(0));
     }
-    //3.添加定时任务
-    @Scheduled(cron = "0/5 * * * * ?")
-    //或直接指定时间间隔，例如：5秒
-    //@Scheduled(fixedRate=5000)
-    private void configureTasks() {
-        System.err.println("执行静态定时任务时间: " + LocalDateTime.now());
-
-    }
+//    //3.添加定时任务
+//    @Scheduled(cron = "0/5 * * * * ?")
+//    //或直接指定时间间隔，例如：5秒
+//    //@Scheduled(fixedRate=5000)
+//    private void configureTasks() {
+//        System.err.println("执行静态定时任务时间: " + LocalDateTime.now());
+//
+//    }
 }
